1f55f3564ad8d303a91b69176851c35b05d4f28d
Todor <TodorStoianov@gmail.com>
2014-04-06 20:49:05 +0300

    Angular and Require added to web client

---
+++ Platform/Clients/Web/app/js/main.js
@@ -0,0 +1,31 @@
+/**
+ * configure RequireJS
+ * prefer named modules to long paths, especially for version mgt
+ * or 3rd party libraries
+ */
+require.config({
+
+    paths: {
+        'angular': '../lib/angular/angular',
+        'angular-route': '../lib/angular-route/angular-route',
+        'domReady': '../lib/requirejs-domready/domReady'
+    },
+
+    /**
+     * for libs that either do not support AMD out of the box, or
+     * require some fine tuning to dependency mgt'
+     */
+    shim: {
+        'angular': {
+            exports: 'angular'
+        },
+        'angular-route': {
+            deps: ['angular']
+        }
+    },
+
+    deps: [
+        // kick start application... see bootstrap.js
+        './bootstrap'
+    ]
+});
---
+++ Platform/Clients/Web/server.bat
@@ -0,0 +1,2 @@
+cd app
+node ..\server\web-server.js
