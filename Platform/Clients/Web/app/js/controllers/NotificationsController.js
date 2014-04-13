d6818c4ff09a5787475045828c618013170a6d65
ivaylokenov <ivaylo.kenov@gmail.com>
2014-04-13 14:11:25 +0300

    Sending notifications implemented

--- Platform/Clients/Web/app/js/controllers/NotificationsController.js
+++ Platform/Clients/Web/app/js/controllers/NotificationsController.js
@@ -1,15 +1,22 @@
 'use strict'
 
 astroPlatform.controller('NotificationsController',
-    function NotificationsController($scope, $cookieStore, notificationsData) {
+    function NotificationsController($scope, $resource, $cookieStore, apiUrl, notificationsData) {
 
         $scope.notifications = [];
 
-        if ($cookieStore.get('notifications')) {
-            $scope.notifications = JSON.parse($cookieStore.get('notifications'));
-        }
+        //if ($cookieStore.get('notifications')) {
+        //    $scope.notifications = JSON.parse($cookieStore.get('notifications'));
+        //}
 
         $scope.sort = '-id';
+
+        $scope.send = function(message, sendMessageForm) {
+            if (sendMessageForm.$valid) {
+                var resource = $resource(apiUrl + '/activities');
+                resource.save(message);
+            }
+        }
 
         notificationsData.socket.onmessage = function(evt) {
             var notification = JSON.parse(evt.data);
