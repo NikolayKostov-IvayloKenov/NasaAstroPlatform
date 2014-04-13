ee24d0239701f2bfab8c7650bf16a09ba8031ade
Nikolay Kostov <admin@nikolay.it>
2014-04-13 14:23:34 +0300

    Pebble app finished

--- Platform/Clients/Pebble/app.js
+++ Platform/Clients/Pebble/app.js
@@ -1,26 +1,25 @@
 console.log('Astro Platform started!');
 var lastMessage = "";
 function update() {
-  // TODO: Set url
-  ajax({ url: 'http://simplyjs.io' }, function(data){
-    var message = data.match(/<h1>(.*?)<\/h1>/)[1];
-    console.log('Message: ' + message + ' ;Last: ' + lastMessage);
+  ajax({ url: 'http://79.124.67.13:8080/activities/last' }, function(data){
+    var message = data;
+    // console.log('Message: ' + message + ' ;Last: ' + lastMessage);
     if (lastMessage != message) {
       lastMessage = message;
-      simply.subtitle(message);
+      simply.subtitle("");
+      simply.body(message);
       simply.vibe();
-    } 
+    }
   });
 }
 
 simply.on('singleClick', function(e) {
-  console.log(util2.format('single clicked $button!', e));
   update();
 });
 
 simply.setText({
-  title: 'Astro Platform',
-  body: 'We are syncing... Please wait...',
+  title: 'AstroPlatform',
+  subtitle: 'We are syncing... Please wait...',
 }, true);
 
-setInterval(update, 1000);
+setInterval(update, 1500);
