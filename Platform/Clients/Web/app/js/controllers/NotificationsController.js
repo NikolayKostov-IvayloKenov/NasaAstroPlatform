56840a18fbbb0849e84d6d627dbd387a68a71241
ivaylokenov <ivaylo.kenov@gmail.com>
2014-04-13 13:36:31 +0300

    Added notifications session

--- Platform/Clients/Web/app/js/controllers/NotificationsController.js
+++ Platform/Clients/Web/app/js/controllers/NotificationsController.js
@@ -1,9 +1,14 @@
 'use strict'
 
 astroPlatform.controller('NotificationsController',
-    function NotificationsController($scope, notificationsData) {
+    function NotificationsController($scope, $cookieStore, notificationsData) {
 
         $scope.notifications = [];
+
+        if ($cookieStore.get('notifications')) {
+            $scope.notifications = JSON.parse($cookieStore.get('notifications'));
+        }
+
         $scope.sort = '-id';
 
         notificationsData.socket.onmessage = function(evt) {
@@ -11,6 +16,7 @@ astroPlatform.controller('NotificationsController',
             $scope.$apply(function(){
                 $scope.notifications.push(notification);
             })
+            $cookieStore.put('notifications', JSON.stringify($scope.notifications));
         }
 
     }
--- Platform/Clients/Web/app/js/services/notificationsData.js
+++ Platform/Clients/Web/app/js/services/notificationsData.js
@@ -2,7 +2,7 @@
 
 astroPlatform.factory('notificationsData', function($resource, apiUrl) {
 
-    var notificationSocket = new WebSocket('ws://localhost:8080');
+    var notificationSocket = new WebSocket('ws://79.124.67.13:8080'); // new WebSocket('ws://localhost:8080');
 
     return {
         socket: notificationSocket
