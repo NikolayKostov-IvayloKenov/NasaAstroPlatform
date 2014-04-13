e3019fd5195988ce0f9405d254ef76ed94b49301
ivaylokenov <ivaylo.kenov@gmail.com>
2014-04-13 14:39:17 +0300

    Master detail fixed

--- Platform/Clients/Web/app/js/services/usersData.js
+++ Platform/Clients/Web/app/js/services/usersData.js
@@ -10,34 +10,13 @@ astroPlatform.factory('usersData', function($resource, apiUrl) {
 })
 
 
-astroPlatform.factory('userProfileData', function($resource) {
+astroPlatform.factory('userProfileData', function($resource, apiUrl) {
 
-    var resource = $resource('/api/astronauts/:username', { username: '@username' });
+    var resource = $resource(apiUrl + '/astronauts/:username', { username: '@username' });
 
     return {
         getUserData: function(username) {
-            var user = {
-                id: 1,
-                username: 'hansen',
-                name: 'Jeremy Hansen',
-                status: true,
-                imageUrl: '/img/hansen.jpg',
-                activities: [{
-                    id: 2,
-                    type: "text",
-                    title: "New Satellite in space",
-                    content: "We sent a new satellite in space today and everything was as smooth as we expected!",
-                    createdOn: new Date(2014, 3, 12, 12, 16, 53, 111).toLocaleString()
-                },{
-                    id: 4,
-                    type: "image",
-                    title: "A view form the cupola",
-                    content: "/img/activities/viewFromCupola_small.jpg",
-                    createdOn: new Date(2014, 3, 13, 9, 16, 18, 111).toLocaleString()
-                }]
-            };
-
-            return user;
+            return resource.get({username: username});
         }
     }
 })
